802a9adab50c1cc77032ce118782f50461e3ad73
Raphael Santos Duarte <raphael-duarte@outlook.com>
2017-04-09 18:27:30 -0300

    Add project files.

---
+++ CargaCotacoes/Carga.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
+
+namespace CargaCotacoes
+{
+    public static class Carga
+    {
+        //método de incluir cotação
+        public static void IncluirCotacao(CloudTable table,
+            string siglaMoeda, string dataCotacao, double valorComercial)
+        {
+            CotacaoEntity cotacao = new CotacaoEntity(siglaMoeda, dataCotacao, valorComercial);
+
+            //criando objeto para operação na tabela
+            TableOperation insertOperation = TableOperation.Insert(cotacao);
+
+            //executa a operação na tabela
+            table.Execute(insertOperation);
+        }
+
+        //método de consulta das cotações
+        public static void ListarCotacoes<T>(CloudTable table, string siglaMoeda) where T: CotacaoEntity, new()
+        {
+            //executando a query
+            TableQuery<T> query = new TableQuery<T>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, siglaMoeda));
+
+            //varendo a tabela
+            foreach (T entity in table.ExecuteQuery(query))
+            {
+                Console.Write(Environment.NewLine);
+                Console.WriteLine(JsonConvert.SerializeObject(entity));
+            }
+            Console.Write(Environment.NewLine);
+        }
+    }
+}
---
+++ CargaCotacoes/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
+
+
+namespace CargaCotacoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Obter configurações de acesso...");
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+
+            Console.WriteLine("Gerar a referência da tabela e criar no storage (caso a mesma não ainda exista)...");
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable table = tableClient.GetTableReference("Cotacoes");
+            table.CreateIfNotExists();
+
+            /*
+            //Incluindo cotação Euro
+            Console.WriteLine("Incluindo cotações do Euro (EUR)...");
+            Carga.IncluirCotacao(table, "EUR", "2017-03-22 16:59", 3.4071);
+            Carga.IncluirCotacao(table, "EUR", "2017-03-23 16:59", 3.3480);
+
+            //Trazendo a cotação do Euro
+            Console.WriteLine("Listando cotações do Euro (EUR)...");
+            Carga.ListarCotacoes<CotacaoEntity>(table, "EUR");
+
+            //Incluindo a cotação da Libra
+            Console.WriteLine("Incluindo cotações da Libra Esterlina (LIB)...");
+            Carga.IncluirCotacao(table, "LIB", "2017-03-22 16:59", 3.7980);
+            Carga.IncluirCotacao(table, "LIB", "2017-03-22 16:59", 3.8589);
+
+            //Trazendo a cotação da Libra
+            Console.WriteLine("Listando cotações da Libra Esterlina (LIB)...");
+            Carga.ListarCotacoes<CotacaoEntity>(table, "LIB");
+
+            //Incluindo cotação do dolar por BATCH
+            Console.WriteLine("Incluindo cotações do Dólar (USD)...");
+            TableBatchOperation batch = new TableBatchOperation();
+            batch.Insert(new CotacaoDolarEntity("USD", "2017-03-22 16:59", 3.0800, 3.3300));
+            batch.Insert(new CotacaoDolarEntity("USD", "2017-03-23 16:59", 3.1000, 3.3600));
+            table.ExecuteBatch(batch);
+            
+            //Trazendo a cotação do dolar
+            Console.WriteLine("Listando cotações do Dólar (USD)...");
+            Carga.ListarCotacoes<CotacaoDolarEntity>(table, "USD");
+
+            Console.WriteLine("Finalizado!");
+            */
+
+            //Trazendo a cotação do Euro
+            Console.WriteLine("Listando cotações do Euro (EUR)...");
+            Carga.ListarCotacoes<CotacaoEntity>(table, "EUR");
+
+            //Trazendo a cotação da Libra
+            Console.WriteLine("Listando cotações da Libra Esterlina (LIB)...");
+            Carga.ListarCotacoes<CotacaoEntity>(table, "LIB");
+
+            //Trazendo a cotação do dolar
+            Console.WriteLine("Listando cotações do Dólar (USD)...");
+            Carga.ListarCotacoes<CotacaoDolarEntity>(table, "USD");
+
+
+            Console.ReadKey();
+        }
+    }
+}
